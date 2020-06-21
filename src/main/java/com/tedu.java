7e36caa7d48f1c3b650138c2841ef0199c118695
yanxinyu <2425453191@qq.com>
2020-06-21 17:30:01 +0800

    first commit

---
+++ src/main/java/com/tedu.java
@@ -0,0 +1,11 @@
+package com;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class tedu {
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(integers);
+    }
+}
